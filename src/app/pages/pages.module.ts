bec1e181004828ff9b9e814e7f67679505452750
Hallison <hallison.o.b@gmail.com>
2023-05-23 15:55:14 -0300

    feat: create poke-list and styles

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { RoutingModule } from './routing.module';
 import { DatailsComponent } from './datails/datails.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [HomeComponent, DatailsComponent],
-  imports: [RoutingModule, CommonModule],
+  imports: [RoutingModule, CommonModule, SharedModule],
 })
 export class PagesModule {}
